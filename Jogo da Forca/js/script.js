b540773fe0a3834ff09ff34e332635206f0d62e0
Guilherme Matos de Oliveira <127865701+GuilhermeM777@users.noreply.github.com>
2023-10-25 11:56:42 -0300

    Adição do Jogo

---
+++ Jogo da Forca/js/script.js
@@ -0,0 +1,15 @@
+let can = document.getElementById ('canvas');
+let forca = can.getContext ('2d');
+let objeto = can.getContext ('2d');
+let canvasWidth = 100;
+let canvasHeight = 100;
+
+    function cenario(){
+objeto.fillStyle = 'brown';
+objeto.fillRect (10, 10, 100, 30);
+    
+
+
+
+
+}
